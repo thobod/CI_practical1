c6dbc7e4f590e23391ec9f79918e1e3ce2df4813
Unknown <t.boddeus@gmail.com>
2018-06-01 13:43:08 +0200

    added some comments

--- CI_practical1/CI_practical1/Field.cs
+++ CI_practical1/CI_practical1/Field.cs
@@ -8,11 +8,13 @@ using System.Threading.Tasks;
 class Field
 {
     //this class keeps track of the value and weither or not the value is fixed.
-    int fieldValue;
-    bool isFixed;
-    int blockX, blockY;
+    int fieldValue; //value of a field
+    bool isFixed;   //weither the value was given in the original problem or generated.
+    int blockX, blockY; //the x and y coordinates of the block this field is a part of, in a 3x3 sudoku, the value at (4,1) would be in block (1,0) the value (7,8) in block (2,2) ect.
+
     public Field(int fieldValue, int blockX, int blockY)
-    {
+    { 
+        //initiating the field.
         this.fieldValue = fieldValue;
         if (fieldValue > 0)
             isFixed = true;
--- CI_practical1/CI_practical1/SudokuBoard.cs
+++ CI_practical1/CI_practical1/SudokuBoard.cs
@@ -14,7 +14,7 @@ class SudokuBoard
     {
         sudoku = new Field[N, N];
         this.N = N;
-        for (int i = 0; i < N; i++)
+        for (int i = 0; i < N; i++) //initiate the board with the given array of ints.
             for (int j = 0; j < N; j++)
             {
                 sudoku[i, j] = new Field(sudokuboard[i, j], (i / (int)Math.Sqrt(N)), (j / (int)Math.Sqrt(N)));
@@ -22,20 +22,20 @@ class SudokuBoard
         randomizeboard();
     }
 
-    public void randomizeboard()
+    public void randomizeboard() //generates a random board, with unique values in each block, and the fixed values staying fixed.
     {
-        for (int x = 0; x < N; x+= (int)Math.Sqrt(N))
+        for (int x = 0; x < N; x+= (int)Math.Sqrt(N)) //for every block in the board:
             for (int y = 0; y < N; y += (int)Math.Sqrt(N))
             {
                 List<int> fixedvalues = new List<int>();
                 int nextValue = 1;
-                for (int i = sudoku[x, y].BlockX * (int)Math.Sqrt(N); i < (sudoku[x, y].BlockX + 1) * (int)Math.Sqrt(N); i++)
+                for (int i = sudoku[x, y].BlockX * (int)Math.Sqrt(N); i < (sudoku[x, y].BlockX + 1) * (int)Math.Sqrt(N); i++) //for every field in the block : check which values are already in use
                     for (int j = sudoku[x, y].BlockY * (int)Math.Sqrt(N); j < (sudoku[x, y].BlockY + 1) * (int)Math.Sqrt(N); j++)
                     {
                         if (sudoku[i, j].FieldValue > 0)
                             fixedvalues.Add(sudoku[i, j].FieldValue);
                     }
-                for (int i = sudoku[x, y].BlockX * (int)Math.Sqrt(N); i < (sudoku[x, y].BlockX + 1) * (int)Math.Sqrt(N); i++)
+                for (int i = sudoku[x, y].BlockX * (int)Math.Sqrt(N); i < (sudoku[x, y].BlockX + 1) * (int)Math.Sqrt(N); i++) //for every field in the block : add a value which isnt in that block yet. 
                     for (int j = sudoku[x, y].BlockY * (int)Math.Sqrt(N); j < (sudoku[x, y].BlockY + 1) * (int)Math.Sqrt(N); j++)
                     {
                         if (!sudoku[i, j].IsFixed)
@@ -46,22 +46,12 @@ class SudokuBoard
                             }
                             sudoku[i, j].FieldValue = nextValue;
                             nextValue++;
-
                         }
-
-
-                        /* if (!fixedvalues.Contains(sudoku[i, j].FieldValue))
-                         {
-                             sudoku[i, j].FieldValue = nextValue;
-                             fixedvalues.Add(nextValue);
-                             nextValue++;
-                         }*/
-
                     }
             }
     }
 
-    public void printSudoku()
+    public void printSudoku() //prints the sudoku to console.
     {
         for (int x = 0; x < N; x++)
         {
